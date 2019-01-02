72248c5d7066c7200b0b154f92b2afad2c2bd7b6
Slawa Pidgorny <spidgorny@gmail.com>
2019-01-02 23:25:38 +0100

    - add SMSParser - add average per month calculation

---
+++ lib/SMSParser.dart
@@ -0,0 +1,180 @@
+import 'dart:convert';
+import 'dart:io';
+
+import 'package:sms/sms.dart';
+
+import 'TransSet.dart';
+import 'Transaction.dart';
+
+class SMSParser {
+  Map<String, List<String>> statusList;
+
+  Map<DateTime, double> states = {};
+  List<Transaction> transactions = [];
+  Map<DateTime, List<Transaction>> perMonth = {};
+
+  SMSParser(this.statusList);
+
+  Future readAndParse() async {
+    print('Querying SMS...');
+    SmsQuery query = new SmsQuery();
+    List<SmsMessage> messages = await query.querySms(kinds: [
+      SmsQueryKind.Inbox,
+      //SmsQueryKind.Sent
+    ], address: 'IhreBank');
+    print('Fetched ${messages.length}');
+
+//    saveMessages(messages);
+
+    DateTime date;
+    for (var m in messages) {
+//      print([m.sender, m.date, m.body].join('\t'));
+
+      var lines;
+      if (m.body.startsWith('KONTOSTAND')) {
+        var datePresplit =
+            m.body.splitMapJoin(RegExp('\\.20\\d\\d'), onMatch: (m) {
+//          print('match $m');
+          return m.group(0) + '///';
+        }, onNonMatch: (m) {
+//          print('non-match $m');
+          return m.toString();
+        });
+//        print(datePresplit);
+        var dateSplit = datePresplit.split('///');
+//        print(dateSplit);
+
+        var parts = dateSplit[0].split(' ');
+
+        try {
+          var sDate = parts[3].split('.').reversed.join('-');
+          date = DateTime.parse(sDate);
+//          print(date);
+        } on RangeError catch (e) {
+          print('Exception $e in [' + parts.toString() + ']');
+          print(m.body);
+        }
+
+        var iState;
+        var sState = parts[2].replaceFirst(',', '.');
+        if (sState.endsWith('-')) {
+          iState = -double.parse(sState.substring(0, sState.length - 1));
+        } else {
+          iState = double.parse(sState);
+        }
+//          print('$date: $iState');
+        this.states[date] = iState;
+
+        lines = dateSplit[1].split(RegExp('\\d-'));
+      } else {
+        lines = m.body.split(RegExp('\\d-'));
+      }
+
+      for (var l in lines) {
+        var trans;
+        if (l.startsWith('SEITE')) {
+          l = l.split(':').sublist(1).join(' ');
+          try {
+            trans = Transaction.parse(date, l);
+//              print(trans);
+          } on FormatException {
+            //skip
+            print('Exception[' + l + ']');
+          }
+        } else {
+          try {
+            trans = Transaction.parse(date, l);
+//              print(trans);
+          } on FormatException {
+            //skip
+            print('Exception[' + l + ']');
+          }
+        }
+
+        if (trans != null) {
+          this.transactions.add(trans);
+        }
+      }
+    }
+    print('Parsed ${transactions.length} transactions');
+  }
+
+  getOrderedTransactionSetsFor(DateTime currentMonth) {
+    var transactionsThisMonth = this.transactions.where((Transaction t) {
+      var date = t.date;
+      if (date.year == currentMonth.year && date.month == currentMonth.month) {
+        return true;
+      }
+      return false;
+    });
+
+    Map<String, TransSet> sets = {};
+    for (var t in transactionsThisMonth) {
+      var code = t.note;
+      for (var each in statusList.keys) {
+        if (statusList[each].contains(code)) {
+          code = '[$each]';
+          break;
+        }
+      }
+      sets[code] = sets[code] ?? TransSet(code);
+      sets[code].add(t);
+    }
+
+    List<TransSet> ordered = sets.values.toList();
+    ordered.sort((a, b) {
+      return a.total > b.total ? -1 : 1;
+    });
+
+//    for (var entry in ordered) {
+//      print(
+//          '${entry.data.first.date.toString()}\t${entry.total}\t${entry.code}');
+//    }
+    return ordered;
+  }
+
+  void saveMessages(List<SmsMessage> messages) {
+    var json = [];
+    for (var m in messages) {
+      json.add({
+        'sender': m.sender,
+        'date': m.date.toString(),
+        'dateSent': m.dateSent.toString(),
+        'body': m.body,
+        'address': m.address,
+        'id': m.id,
+        'threadId': m.threadId,
+      });
+    }
+    File f = File('/storage/emulated/0/Download/sms.json');
+    f.writeAsStringSync(jsonEncode(json));
+  }
+
+  void buildPerMonth() {
+    for (var t in transactions) {
+      var month = DateTime(t.date.year, t.date.month, 1);
+      perMonth[month] = perMonth[t.date] ?? [];
+      perMonth[month].add(t);
+    }
+  }
+
+  double getAverageFor(String note) {
+    perMonth ?? buildPerMonth();
+    List<double> sumPerMonth = [];
+    for (var monthData in perMonth.values) {
+      double sum = 0;
+      for (var t in monthData) {
+        if (t.note == note) {
+          sum += t.amount;
+        }
+      }
+      if (sum > 0) {
+        sumPerMonth.add(sum);
+      }
+    }
+    if (sumPerMonth.length > 0) {
+      return sumPerMonth.reduce((total, el) => total + el) / sumPerMonth.length;
+    }
+    return 0;
+  }
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,9 @@
 import 'package:flutter/material.dart';
 import 'package:localstorage/localstorage.dart';
-import 'package:sms/sms.dart';
 
+import 'SMSParser.dart';
 import 'TransSet.dart';
 import 'TransSetView.dart';
-import 'Transaction.dart';
 
 void main() => runApp(MyApp());
 
@@ -32,8 +31,6 @@ class MyHomePage extends StatefulWidget {
 
 class _MyHomePageState extends State<MyHomePage> {
   DateTime currentMonth = DateTime.now();
-  Map<DateTime, double> states = {};
-  List<Transaction> transactions = [];
   List<TransSet> orderedTransactionSets = [];
   Map<String, List<String>> statusList = {
     'Food': [],
@@ -42,6 +39,7 @@ class _MyHomePageState extends State<MyHomePage> {
     'Restaurant': [],
   };
   final LocalStorage storage = new LocalStorage('MyHomePage');
+  SMSParser parser;
 
   @override
   void initState() {
@@ -61,123 +59,17 @@ class _MyHomePageState extends State<MyHomePage> {
   }
 
   void fetchAndParseSMS() async {
-    transactions = [];
     orderedTransactionSets = [];
     setState(() {}); // show loading indicator
 
-    SmsQuery query = new SmsQuery();
-    List<SmsMessage> messages = await query.querySms(kinds: [
-      SmsQueryKind.Inbox,
-      //SmsQueryKind.Sent
-    ], address: 'IhreBank');
-//    print(messages.length.toString() + ' ' + currentMonth.toString());
-
-    DateTime date;
-    for (var m in messages) {
-//      print([m.sender, m.date, m.body].join('\t'));
-
-      var lines;
-      if (m.body.startsWith('KONTOSTAND')) {
-        var datePresplit =
-            m.body.splitMapJoin(RegExp('\\.20\\d\\d'), onMatch: (m) {
-//          print('match $m');
-          return m.group(0) + '///';
-        }, onNonMatch: (m) {
-//          print('non-match $m');
-          return m.toString();
-        });
-//        print(datePresplit);
-        var dateSplit = datePresplit.split('///');
-//        print(dateSplit);
-
-        var parts = dateSplit[0].split(' ');
-
-        try {
-          var sDate = parts[3].split('.').reversed.join('-');
-          date = DateTime.parse(sDate);
-//          print(date);
-        } on RangeError catch (e) {
-          print('Exception $e in [' + parts.toString() + ']');
-          print(m.body);
-        }
-
-        var iState;
-        var sState = parts[2].replaceFirst(',', '.');
-        if (sState.endsWith('-')) {
-          iState = -double.parse(sState.substring(0, sState.length - 1));
-        } else {
-          iState = double.parse(sState);
-        }
-//          print('$date: $iState');
-        this.states[date] = iState;
-
-        lines = dateSplit[1].split(RegExp('\\d-'));
-      } else {
-        lines = m.body.split(RegExp('\\d-'));
-      }
-
-      for (var l in lines) {
-        var trans;
-        if (l.startsWith('SEITE')) {
-          l = l.split(':').sublist(1).join(' ');
-          try {
-            trans = Transaction.parse(date, l);
-//              print(trans);
-          } on FormatException catch (e) {
-            //skip
-            print('Exception[' + l + ']');
-          }
-        } else {
-          try {
-            trans = Transaction.parse(date, l);
-//              print(trans);
-          } on FormatException catch (e) {
-            //skip
-            print('Exception[' + l + ']');
-          }
-        }
-
-//        print(date.year.toString() +
-//            ' ' +
-//            currentMonth.year.toString() +
-//            ' ' +
-//            date.month.toString() +
-//            ' ' +
-//            currentMonth.month.toString());
-        if (trans != null &&
-            date.year == currentMonth.year &&
-            date.month == currentMonth.month) {
-//          print(trans);
-          this.transactions.add(trans);
-        }
-      }
+    if (parser == null) {
+      parser = SMSParser(statusList);
+      await parser.readAndParse();
     }
-
-    Map<String, TransSet> sets = {};
-    for (var t in transactions) {
-      var code = t.note;
-      for (var each in statusList.keys) {
-        if (statusList[each].contains(code)) {
-          code = '[$each]';
-          break;
-        }
-      }
-      sets[code] = sets[code] ?? TransSet(code);
-      sets[code].add(t);
-    }
-
-    List<TransSet> ordered = sets.values.toList();
-    ordered.sort((a, b) {
-      return a.total > b.total ? -1 : 1;
-    });
-
-//    for (var entry in ordered) {
-//      print(
-//          '${entry.data.first.date.toString()}\t${entry.total}\t${entry.code}');
-//    }
 
     setState(() {
-      orderedTransactionSets = ordered;
+      orderedTransactionSets =
+          parser.getOrderedTransactionSetsFor(currentMonth);
     });
   }
 
@@ -209,9 +101,11 @@ class _MyHomePageState extends State<MyHomePage> {
                           ),
                           title: Text(
                             set.code,
-                            style: Theme.of(context).textTheme.title,
+                            style: Theme.of(context).textTheme.subtitle,
                           ),
-                          subtitle: chips,
+                          subtitle: Text(parser
+                              .getAverageFor(set.code)
+                              .toStringAsFixed(2)),
                           trailing: Text(set.total.toStringAsFixed(2)),
                           dense: true,
                           onTap: () {
